968140d41dd8d85db318f88b50c57c6d3ea737f1
Steven Rogers <srogers1@gmail.com>
2016-11-15 00:05:51 -0600

    Mimic the HTML structure inside the participants DIV - update individual users as they come and go

--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -3,4 +3,13 @@ class RoomUser < ApplicationRecord
   belongs_to :room
   belongs_to :user
 
+  # Currently, these correspond to classes in the view "connection-xxx"
+  NEVER   = "never"
+  UP      = "up"
+  DOWN    = "down"
+  WARNING = "warning"
+
+  STATUSES = [ NEVER, UP, DOWN, WARNING ]
+
+  # Does anything else need to live here?
 end
